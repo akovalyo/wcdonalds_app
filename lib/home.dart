c269b7eae3a8f523019dd21228a556dec0a789e0
Alexander Kovalyov <al.kovalyov@gmail.com>
2022-02-22 20:43:30 -0600

    Add AppState

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import './tabs/tabs.dart';
+import 'package:provider/provider.dart';
+
+import 'tabs/tabs.dart';
+import 'models/app_state.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,31 +12,51 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  void _onTabTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: ImageIcon(
-          AssetImage('assets/images/logo_y.png'),
-          color: Theme.of(context).colorScheme.secondary,
+    double height = MediaQuery.of(context).size.height;
+
+    return Consumer<AppState>(builder: (context, appState, child) {
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: ImageIcon(
+            const AssetImage('assets/images/logo_y.png'),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
         ),
-      ),
-      body: Tabs.listWidgets()[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-        currentIndex: _selectedIndex,
-        onTap: _onTabTapped,
-        items: Tabs.listNavigationBarItems(),
-      ),
-    );
+        drawer: Drawer(
+          child: SafeArea(
+            child: ListView(
+              children: [
+                Container(
+                  height: height * 0.2,
+                  color: Theme.of(context).appBarTheme.backgroundColor,
+                  child: const Align(
+                    alignment: Alignment.center,
+                    child: Image(
+                      width: 50,
+                      height: 50,
+                      image: AssetImage('assets/images/logo_y.png'),
+                    ),
+                  ),
+                ),
+                const Divider(),
+              ],
+            ),
+          ),
+        ),
+        body: Tabs.listWidgets()[appState.selectedTab],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor:
+              Theme.of(context).textSelectionTheme.selectionColor,
+          currentIndex: appState.selectedTab,
+          onTap: (index) {
+            appState.goToTab(index);
+          },
+          items: Tabs.listNavigationBarItems(),
+        ),
+      );
+    });
   }
 }
