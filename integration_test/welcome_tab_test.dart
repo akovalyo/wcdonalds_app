ccb668e23f4d21e5e48ddc8d6d92c51e860f15f0
Alexander Kovalyov <al.kovalyov@gmail.com>
2022-03-10 01:49:18 -0600

    Add tests for WorkerImageTile on the

--- integration_test/welcome_tab_test.dart
+++ integration_test/welcome_tab_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 import 'package:wcdonalds_app/main.dart' as app;
 
@@ -17,10 +16,18 @@ void main() {
     const String franchiseOne = 'assets/images/franchise_1.jpeg';
     const String franchiseTwo = 'assets/images/franchise_2.jpeg';
 
-    void evaluateWorkerImageTile(Key key) {}
+    Future<ImageProvider> evaluateWorkerImageTile(Key key) async {
+      final Finder workerImageFinder = find.descendant(
+          of: find.byKey(key), matching: find.byType(FadeInImage));
+      expect(workerImageFinder, findsOneWidget);
 
-    testWidgets('find images and text on the page',
-        (WidgetTester tester) async {
+      final FadeInImage workerImage =
+          workerImageFinder.evaluate().single.widget as FadeInImage;
+
+      return workerImage.image;
+    }
+
+    testWidgets('Test welcome page', (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
 
@@ -34,35 +41,38 @@ void main() {
       await tester.pumpAndSettle();
       expect(find.image(const AssetImage(franchiseTwo)), findsOneWidget);
 
+      // Test WorkerImageTiles
+      expect(
+          await evaluateWorkerImageTile(
+            const Key('workerImageOne'),
+          ),
+          const NetworkImage(workerOneImage));
+      expect(
+          await evaluateWorkerImageTile(
+            const Key('workerImageTwo'),
+          ),
+          const NetworkImage(workerTwoImage));
+
       final Finder workerImageOneTileFinder =
           find.byKey(const Key('workerImageOne'));
-      final Finder workerImageOneFinder = find.descendant(
-          of: find.byKey(const Key('workerImageOne')),
-          matching: find.byType(FadeInImage));
-      expect(workerImageOneFinder, findsOneWidget);
-      final FadeInImage workerImageOne =
-          workerImageOneFinder.evaluate().single.widget as FadeInImage;
-      print(workerImageOne.image);
-      // expect(find.text('0'), findsOneWidget);
-      // Verify the counter starts at 0.
-      // final workerImages = find.byType(NetworkImage) as List;
-      // print(workerImages);
-      // expect(find.byType(NetworkImage), findsNWidgets(2));
-      // expect(find.image(FileImage(File(workerOneImage))), findsOneWidget);
+      await tester.tap(workerImageOneTileFinder);
+      await tester.pumpAndSettle();
 
-      // expect(find.image(FileImage(File(workerTwoImage))), findsOneWidget);
+      expect(
+          await evaluateWorkerImageTile(
+            const Key('workerImageOne'),
+          ),
+          isNot(const NetworkImage(workerOneImage)));
 
-      // Finds the floating action button to tap on.
-      // final Finder fab = find.byTooltip('Increment');
-
-      // Emulate a tap on the floating action button.
-      // await tester.tap(fab);
-
-      // Trigger a frame.
-      // await tester.pumpAndSettle();
-
-      // // Verify the counter increments by 1.
-      // expect(find.text('1'), findsOneWidget);
+      final Finder workerImageTwoTileFinder =
+          find.byKey(const Key('workerImageTwo'));
+      await tester.tap(workerImageTwoTileFinder);
+      await tester.pumpAndSettle();
+      expect(
+          await evaluateWorkerImageTile(
+            const Key('workerImageTwo'),
+          ),
+          isNot(const NetworkImage(workerTwoImage)));
     });
   });
 }
