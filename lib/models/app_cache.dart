19dec14c1d0961022e245733b2380fcf1b8b4116
Alex Kovalyov <al.kovalyov@gmail.com>
2022-04-28 11:16:59 -0500

    Add saveList and getList to AppCache

--- lib/models/app_cache.dart
+++ lib/models/app_cache.dart
@@ -2,6 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AppCache {
   static const darkTheme = 'darkTheme';
+  static const workersList = 'workersList';
 
   Future<bool> isDarkTheme() async {
     final prefs = await SharedPreferences.getInstance();
@@ -11,5 +12,15 @@ class AppCache {
   Future<void> saveTheme(bool val) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(darkTheme, val);
+  }
+
+  Future<void> saveList(String key, List<String> data) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList(key, data);
+  }
+
+  Future<List<String>?> getList(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(key);
   }
 }
